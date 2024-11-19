c8d80646180e999aac51bc95643045b91ecb0171
ymoumine <ymoum044@uottawa.ca>
2024-11-18 21:41:20 -0500

    installation scripts

---
+++ install_ros_companion.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/bash
+
+# check locale for UTF-8
+if echo $(locale) | grep -q "UTF-8"; then
+    echo "UTF-8 already enabled"
+else
+    sudo apt update && sudo apt install locales
+    sudo locale-gen en_US en_US.UTF-8
+    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+    export LANG=en_US.UTF-8
+
+    locale
+fi
+
+# enable required repositories
+
+# ubuntu universe repository
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+
+# ROS 2 GPG
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# add the ROS 2 apt repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# dev tools
+sudo apt update && sudo apt install ros-dev-tools
+
+# install ros 2
+sudo apt update && sudo apt full-upgrade && sudo apt install -y ros-jazzy-ros-base
+
---
+++ install_ros_topside.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/bash
+
+# check locale for UTF-8
+if locale | grep -q "UTF-8"; then
+    echo "UTF-8 already enabled"
+else
+    sudo apt update && sudo apt install locales
+    sudo locale-gen en_US en_US.UTF-8
+    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+    export LANG=en_US.UTF-8
+
+    locale
+fi
+
+# enable required repositories
+
+# ubuntu universe repository
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+
+# ROS 2 GPG
+sudo apt update && sudo apt install -y curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# add the ROS 2 apt repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# dev tools
+sudo apt update && sudo apt install -y ros-dev-tools
+
+# install ros 2
+sudo apt update && sudo apt full-upgrade -y && sudo apt install -y ros-jazzy-desktop
+
+# check bash source then append it
+# source /opt/ros
